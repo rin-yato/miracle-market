0a7e91fd989f163ebd69e6615436423a58c623ce
rin-yato <chearithorn@gmail.com>
2023-09-10 00:01:51 +0700

    feat: added placeholder pages

---
+++ apps/admin/src/lib/components/header.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { IconBell, IconHelp } from '@tabler/icons-svelte';
+  import { Button } from './ui/button';
+  import Badge from './ui/badge/badge.svelte';
+</script>
+
+<header class="bg-white flex justify-between items-center py-3 px-5">
+  <Button variant="ghost" class="group text-muted-foreground">
+    <Badge variant="outline" class="px-1">
+      <span class="text-xs duration-200 group-hover:text-foreground">
+        ⌘K
+      </span>
+    </Badge>
+    <span class="mx-1.5">Open Command</span>
+  </Button>
+  <nav>
+    <Button variant="ghost" size="icon">
+      <IconHelp size={20} />
+    </Button>
+    <Button variant="ghost" size="icon">
+      <IconBell size={20} />
+    </Button>
+  </nav>
+</header>
---
+++ apps/admin/src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Header from '$lib/components/header.svelte';
+  import Sidebar from '$lib/components/sidebar.svelte';
+  import '../app.postcss';
+</script>
+
+<div class="flex min-h-screen">
+  <Sidebar />
+  <div class="flex-1">
+    <Header />
+    <slot />
+  </div>
+</div>
