40f204e4eb8a2f728e0975a2a0c32acb75ae02fd
rin-yato <chearithorn@gmail.com>
2023-09-26 14:57:32 +0700

    fix(code-scan): non null assertion

--- apps/api/drizzle.config.ts
+++ apps/api/drizzle.config.ts
@@ -9,6 +9,6 @@ export default {
   strict: true,
   driver: 'pg',
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL!,
+    connectionString: process.env.DATABASE_URL ?? '',
   },
 } satisfies Config;
